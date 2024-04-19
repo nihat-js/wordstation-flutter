c5bb774d5599c91d3aa26f2d92f9cbae23c8d19f
nihat-js <abdullazadenihat@gmail.com>
2024-04-19 04:17:27 +0400

    character added

--- lib/src/screens/game_screen.dart
+++ lib/src/screens/game_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:wordstation_flutter/src/components/circular_joystick.dart';
 import 'package:wordstation_flutter/src/components/status_bar.dart';
 
@@ -33,7 +36,135 @@ class GameScreen extends StatelessWidget {
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
           StatusBar(),
           CircularJoystick(),
+          Character(),
         ]),
+      ),
+    );
+  }
+}
+
+class Character extends StatefulWidget {
+  const Character({super.key});
+
+  @override
+  State<Character> createState() => _CharacterState();
+}
+
+class _CharacterState extends State<Character> {
+  bool isRunning = false;
+
+  double positionX = 0;
+  double positionY = 0;
+
+  double moveSpeed = 20;
+  double mapOffset = 0;
+  List<String> images = [
+    "images/plx-2.png",
+    "images/plx-3.png",
+    "images/plx-4.png"
+  ];
+
+  double screenWidth = 500;
+  double imageWidth = 300;
+
+  double minimumPositionX = 100;
+// 0 => 300 200,  50 => 250 250, 250 => 50,300,150, 700=>
+
+  List<Widget> renderBackgroundWidgets() {
+    List<Widget> widgets = [];
+    int x = max((positionX / imageWidth).ceil(), 1);
+    double offset = positionX % imageWidth;
+
+    double firstImageWidth = imageWidth - offset;
+    double secondImageWidth = min(screenWidth - firstImageWidth, imageWidth);
+    double leftImageWidth = screenWidth - secondImageWidth - firstImageWidth;
+    double thirdImageWidth = leftImageWidth > 0 ? leftImageWidth : 0;
+
+    print(x);
+    print(firstImageWidth);
+    print(secondImageWidth);
+    print(thirdImageWidth);
+
+    widgets.add(
+      Positioned(
+        top: 0,
+        left: 0,
+        width: firstImageWidth,
+        height: 200,
+        child: Image(
+          image: AssetImage(images[x - 1 % images.length]),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+
+    widgets.add(
+      Positioned(
+        top: 0,
+        left: firstImageWidth,
+        width: secondImageWidth,
+        height: 200,
+        child: Image(
+          image: AssetImage(images[(x - 1 + 1) % images.length]),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+
+    if (thirdImageWidth > 0) {
+      widgets.add(
+        Positioned(
+          top: 0,
+          left: firstImageWidth + secondImageWidth,
+          width: thirdImageWidth,
+          height: 200,
+          child: Image(
+            image: AssetImage(images[(x - 1 + 2) % images.length]),
+            fit: BoxFit.cover,
+          ),
+        ),
+      );
+    }
+
+    return widgets;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: screenWidth,
+      height: 200,
+      child: Container(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Stack(
+            children: [
+              ...renderBackgroundWidgets(),
+              Image(
+                width: 52,
+                height: 52,
+                image: AssetImage(isRunning
+                    ? 'images/character-run.gif'
+                    : "images/character-idle.gif"),
+              ),
+              GestureDetector(
+                onTapDown: (TapDownDetails details) {
+                  setState(() {
+                    isRunning = true;
+                    positionX += moveSpeed;
+                  });
+                },
+                onTapCancel: () {
+                  setState(() {
+                    isRunning = false;
+                  });
+                },
+                child: ElevatedButton(
+                    onPressed: () {}, child: Icon(Icons.arrow_right)),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
